ccc2ebbb0d00b38c00e63162553d3c4f071dcea4
Nguyen Gia Huy <ghgaming1234563@gmail.com>
2026-08-14 13:37:37 +0700

    Update run-frontend-apis.cmd

--- run-frontend-apis.cmd
+++ run-frontend-apis.cmd
@@ -2,3 +2,4 @@
 cd /d "%~dp0"
 start "Identity API" cmd /k dotnet run --project "src\Services\Identity\StealDeal.Services.Identity.API\StealDeal.Services.Identity.API.csproj" --launch-profile http
 start "Store API" cmd /k dotnet run --project "src\Services\Store\StealDeal.Services.Store.API\StealDeal.Services.Store.API.csproj" --launch-profile http
+start "Order API" cmd /k dotnet run --project "src\Services\Order\StealDeal.Services.Order.API\StealDeal.Services.Order.API.csproj" --launch-profile http
