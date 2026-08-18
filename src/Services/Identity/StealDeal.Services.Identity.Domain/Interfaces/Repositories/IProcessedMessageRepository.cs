00b93ff0451c640224a8b6ef5195c48a81843042
TaiPhat25 <phattainguyen04w7@gmail.com>
2026-08-18 13:27:15 +0700

    feat(identity): add idempotent seller promotion and RabbitMQ retry limits

---
+++ src/Services/Identity/StealDeal.Services.Identity.Domain/Interfaces/Repositories/IProcessedMessageRepository.cs
@@ -0,0 +1,14 @@
+﻿using StealDeal.Services.Identity.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StealDeal.Services.Identity.Domain.Interfaces.Repositories
+{
+    public interface IProcessedMessageRepository
+    {
+        Task AddAsync(ProcessedMessage processedMessage);
+        Task<bool> ExistsAsync(Guid messageId, string consumerName);
+        Task<ProcessedMessage?> GetAsync(Guid messageId, string consumerName);
+    }
+}
---
+++ src/Services/Identity/StealDeal.Services.Identity.Domain/Models/ProcessedMessage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StealDeal.Services.Identity.Domain.Models
+{
+    public class ProcessedMessage
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public Guid MessageId { get; set; }
+        public string ConsumerName { get; set; } = null!;
+        public string EventType { get; set; } = null!;
+        public Guid? AggregateId { get; set; }
+        public DateTime ProcessedAt { get; set; }
+
+    }
+}
